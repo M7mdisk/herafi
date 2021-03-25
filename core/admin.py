d091959c3d35ff3849b02410bf0fcce8429710d2
M7mdIsk <M7md.k.isk@gmail.com>
2021-03-25 17:23:40 +0300

    Refactored models, improved worker model

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,13 @@
 from django.contrib.gis.admin import OSMGeoAdmin
 from django.contrib import admin
-from .models import Profile, Review
+from .models import Profile, Worker, Review
 
 # Register your models here.
-@admin.register(Profile)
-class ProfileAdmin(OSMGeoAdmin):
-    list_display = ('user','location')
+@admin.register(Worker)
+class WorkerAdmin(OSMGeoAdmin):
+    list_display = ('__str__','profile','location')
+    fields=("profile","phone_number","bio","location","address","city","professions","rating_average")
+    readonly_fields = ("rating_average")
 
+admin.site.register(Profile)
 admin.site.register(Review)
--- core/models.py
+++ core/models.py
@@ -7,24 +7,37 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        User, primary_key=True, on_delete=models.CASCADE, related_name="profile")
-    avatar = models.ImageField()
+        User, primary_key=True, on_delete=models.CASCADE, related_name="user")
+    avatar = models.ImageField(null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+class Worker(models.Model):
+    profile = models.OneToOneField(Profile,primary_key=True, on_delete=models.CASCADE, related_name="profile")
     phone_number = PhoneNumberField()
-    bio = models.TextField()
-    location = models.PointField(null=True)
+    bio = models.TextField(blank=True)
+    location = models.PointField()
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
     professions = models.CharField(max_length=50)
-    n_customers = models.PositiveIntegerField()
-    n_likes = models.PositiveIntegerField()
 
+    @property
+    def ratings(self):
+        return Review.objects.filter(reviewee=self)
+    @property
+    def rating_average(self):
+        return sum([x.rating for x in self.ratings])/len(self.ratings)
+
+    def __str__(self):
+        return f'Worker {self.profile.user.username}'
 
 class Review(models.Model):
     reviewer = models.ForeignKey(
-        Profile, on_delete=models.CASCADE, related_name="reviewer")
+        Profile, on_delete=models.CASCADE, blank=True,null=True,related_name="reviewer")
     reviewee = models.ForeignKey(
-        Profile, on_delete=models.CASCADE, related_name="reviewee")
+        Worker, on_delete=models.CASCADE, related_name="reviewee")
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     review_text = models.TextField(max_length=100)
