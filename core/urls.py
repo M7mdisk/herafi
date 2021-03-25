2c57fdc868e4ffb48a0db58bec22cbac896d5ee8
vreoo <nibrasshami2002@gmail.com>
2021-03-26 01:33:48 +0300

    Adding views and .html files in template dir

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register')
+    # path('figma/', views.figma, name='figma')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Profile, Review, Worker
 # Create your views here.
 
-
 def index(request):
-    return HttpResponse("<h1>loooooooooooooool</h1>")
+    return render(request, 'base.html')
+
+def home(request):    
+    return render(request, 'home.html')
+    # return HttpResponse("<h1>Home</h1>")
+
+def about(request):
+    return render(request, 'about.html')
+
+def figma(request):
+    return render(request, 'figma.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
